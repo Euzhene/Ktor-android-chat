d3b0f0c4d09fa703ae847b017052620f3b62233a
Zhenya20062 <matveeveech1986@yandex.ru>
2022-06-04 17:56:39 +0300

    chat socket service

--- app/src/main/java/com/euzhene/ktorandroidchat/data/remote/ChatSocketService.kt
+++ app/src/main/java/com/euzhene/ktorandroidchat/data/remote/ChatSocketService.kt
@@ -1,12 +1,11 @@
 package com.euzhene.ktorandroidchat.data.remote
 
-import com.euzhene.ktorandroidchat.data.remote.dto.MessageDto
 import com.euzhene.ktorandroidchat.domain.model.Message
 import kotlinx.coroutines.flow.Flow
 
 interface ChatSocketService {
-    suspend fun initSession():Result<Unit>
+    suspend fun initSession(): Result<Unit>
     suspend fun sendMessage()
-    fun observeMessages(): Flow<Result<MessageDto>>
+    fun observeMessages(): Flow<Message>
     suspend fun closeSession()
 }
